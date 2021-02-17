f6df36348117f8a039ada01a4b72e9c1818dd415
Limsokpich Sung <limsung@ucdavis.edu>
2021-02-16 20:17:11 -0800

    final every picture

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -2,95 +2,95 @@
     'use strict';
     console.log('reading js');
 
-    var playBtn = document.querySelector('.contain');
-    var overlay = document.querySelector('#overlay');
-    var overImg = document.querySelector('div img')
-    
-    //when user clicks play button
-    playBtn.addEventListener('click', function (event) {
+    var playBtn = document.querySelector('button');
+    var painting = document.querySelector('#painting');
+    var intro = document.querySelector('#intro');
+
+    var story1 = document.getElementById('story1');
+    var story2 = document.getElementById('story2');
+    var story3 = document.getElementById('story3');
+    var story4 = document.getElementById('story4');
+    var footer = document.getElementById('footer');
+
+    //when you clicks play button
+    playBtn.addEventListener('click', function(event) {
 
         event.preventDefault();
-
-        document.getElementById('overlay').className = 'showing'; 
+        intro.className = 'hidden';
+        painting.className = 'showing';
+        story1.className = 'zoomIn';
+        footer.className = 'showing';
     });
 
-    //when user clicks the x button, overlay disappear
-    document.querySelector('.corner').addEventListener('click', function (event) { 
-        
-        event.preventDefault();
-
-        document.getElementById('overlay').className = 'hidden';
-    });
-
-     //when user press the escape key, the overlays content is hidden again
-     document.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape') {
-            document.querySelector('.showing').className = 'overlay hidden';
-        }
-    });
-
-}());
- 
-/* -just change the .src of the image source when you scroll on js
--use the kitten file but instead of linking it to change the color
-use it to change to when your change image on scroll  */
-
-window.addEventListener('load', function() {
-    'use strict';
-
-    const allImg = document.querySelectorAll('img');
-    const sections = document.querySelectorAll('section'); //grab all the sections from html
+    const sections = document.querySelectorAll('section'); 
     let postTops = []; //create an array to store the location number of each images on the page
     let pageTop; //keep track of how far we are from the top of the page
-    let counter = 0; //index position of the array 
-    let prevCounter = 0;
-    //let doneResizing;
+    let counter = 1; //index position of the array 
 
-    //how we get the console to print the location of each image exactly where it is on relation to the page not the window
-    console.log(sections[0].getBoundingClientRect().top) + window.pageYOffset; 
+    //when the page is reloaded or when the user clicks the go back button the scroll function still works instead of it alreadying loading on the screen 
+    resetPagePosition();
 
-    //this will get the location number for each image and add it to postTops
-    sections.forEach(function(post) { //post is a variable parameter
-        postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset); //make sure that the number we get is rounded down
-    });
-    console.log(postTops); 
+    var lastScrollTop = 0;
 
-    //function for scrolling
-    document.addEventListener('scroll', function() {
-        pageTop = window.pageYOffset;
-        console.log(pageTop);
-
-        //viewpoint option during scrolling when it gets to a certain section print out how many section(images) it has been scrolled down/up
-        if (pageTop > postTops[counter]) {
-            counter++;
-            console.log(`scrolling down ${counter}`);
-        }
-        else if (counter > 0 && pageTop < postTops[counter-1]) {
-            counter--;
-            console.log(`scrolling up ${counter}`);
-        }
-        /* //when the section changes...
-		if (counter != prevCounter) {
-			// changes the class name on the image, which activates animation...
-			document.querySelector('figure img').className = 'sect' + counter;
-			prevCounter = counter;
-		} */
+    window.addEventListener('scroll', function() {
         
-        //zoomIn/zoomOut when scroll you scroll halfway through each picture
-        if (pageTop == postTops[1]/2) {
-            document.getElementsByClassName('tree').className = 'zoomOut';
-            document.getElementsByClassName('car').className = 'zoomIn';
+        // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+        var pageTop = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+        if (pageTop > lastScrollTop){
+            // downscroll code for zoomin/zoomout
+            if (pageTop <= 79) { 
+                story1.className = 'zoomIn';
+            }
+            else if (pageTop <= 824) { 
+                story2.className = 'zoomIn';
+            }
+            else if (pageTop <= 1580) {
+                story3.className = 'zoomIn';
+            }
+            else if (pageTop > 1580) {
+                story4.className = 'zoomIn';
+            }
+        } 
+        else {
+            // upscroll code
+            if (pageTop <= 79) { 
+                story1.className = 'zoomIn';
+            }
+            else if (pageTop <= 824) { 
+                story2.className = 'zoomOut';
+             }
+            else if (pageTop <= 1580) {
+                story3.className = 'zoomOut';
+            }
+            else if (pageTop > 1580) {
+                story4.className = 'zoomOut';
+            }
         }
-        if (pageTop == postTops[2] + 386) {
-            document.getElementsByClassName('car').className = 'zoomOut';
-            document.getElementsByClassName('lake').className = 'zoomIn';
-        }
-        if (pageTop == postTops[3] + 386) {
-            document.getElementsByClassName('lake').className = 'zoomOut';
-            document.getElementsByClassName('mperson').className = 'zoomIn';
-        }
+        lastScrollTop = pageTop <= 0 ? 0 : pageTop; // For Mobile or negative scrolling
+    }, false);
+
+    //when you clicks back button
+    var backBtn = document.getElementById('back');
+
+    backBtn.addEventListener('click', function(event) {
+
+        event.preventDefault();
+        intro.className = 'showing';
+        painting.className = 'hidden';
+        footer.className = 'hidden';
     });
 
-}); 
+    function resetPagePosition() {
+        postTops = [];
+        sections.forEach(function (post) {
+            postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
+        });
 
-   
+        const pagePosition = window.pageYOffset;
+        counter = 0;
+
+        postTops.forEach(function (post) { if (pagePosition > post) { counter++; } });
+
+    }
+
+}());
