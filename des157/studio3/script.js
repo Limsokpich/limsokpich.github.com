c59e0cc2bb484fa35d40213833baeef5d5b40749
Limsokpich Sung <limsung@ucdavis.edu>
2021-03-02 03:23:13 -0800

    update studio3

--- des157/studio3/script.js
+++ des157/studio3/script.js
@@ -2,52 +2,40 @@
     'use strict'
     console.log('reading js');
 
-    var playBtn = document.getElementById('play');
-    var startPage = document.getElementById('startup');
-    var afterClick = document.getElementById('afterclick');
+    var intro = document.getElementById('intro');
+    var card = document.getElementById('card');
+    var startGame = document.getElementById('startgame');
     var footer = document.getElementById('footer');
-    var gameBlocks = document.getElementById('gameblocks');
-    var nextBtn = document.getElementById('next');
 
     // variables for sounds
     const playSound = new Audio('media/play.mp3');
     const rollSound = new Audio('media/roll.mp3');
     const thankSound = new Audio('media/thanksforplaying.mp3');
-
-    // when play button is clicked
-    playBtn.addEventListener('click', function() {
-
-        playSound.play();
-
-        startPage.className = 'hidden';
-        afterClick.className = 'showing';
-    });
-
+   
     // when you hover over the play button, body bg color changes 
     var body = document.body;
 
-    playBtn.onmouseover = function() {
+    startGame.onmouseover = function() {
         body.className = 'hovered';
     };
 
-    playBtn.onmouseout = function() {
+    startGame.onmouseout = function() {
         body.className = '';
-    };
+    }; 
 
-    // when next button is clicked
-    nextBtn.addEventListener('click', function() {
-        startPage.className = 'hidden';
-        afterClick.className = 'hidden';
-        gameBlocks.className = 'showing';
-        footer.className = 'showing';
+    // play button is clicked
+    startGame.addEventListener('click', function() {
+
+        playSound.play();
+
+        intro.className = 'hidden';
+        card.className = 'showing';
+        footer.className = 'showing'; 
     });
 
-
-    var startGame = document.getElementById('startgame');
-    var gameControl = document.getElementById('gamecontrol');
     var game = document.getElementById('game');
-    var score = document.getElementById('score');
     var actionArea = document.getElementById('actions');
+    var score = document.getElementById('score');
 
     // keeping track of game data 
     var gameData = {
@@ -61,13 +49,10 @@
         gameEnd: 29 
     };
 
-    // start the game
+    // rolling start of the game
     startGame.addEventListener('click', function() {
-
+        
         gameData.index = Math.round(Math.random ()); // randomly set game index 
-        gameControl.innerHTML = '<h2 style="color:black;">The Game Has Started</h2>';
-        gameControl.innerHTML += '<button id="how">How To Play?</button>'
-        gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
 
         document.getElementById('quit').addEventListener('click', function () {
             // thanks sound play
@@ -76,31 +61,32 @@
         });
 
         document.getElementById('how').addEventListener('click', function () {
-            afterClick.className = 'showing';
-            nextBtn.remove();
+            intro.className = 'showing';
+            startGame.remove();
+            document.getElementById('random_pick').remove();
         });
 
         console.log(gameData.index);
-        setUpTurn();
+        setUpTurn(); //set up turn appears when you call it in this function 
     });
 
     // setting up the turns
     function setUpTurn() {
-        game.innerHTML = `<h3>Roll the dice, ${gameData.players[gameData.index]}</h3>`;
+        game.innerHTML = `<h2 id="tell_player">Roll the dice, ${gameData.players[gameData.index]}</h2>`;
         actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
         document.getElementById('roll').addEventListener('click', function () {
             throwDice();
             // roll sound play
             rollSound.play();
         });
-    }
+    };
 
     // Throwing the dice 
     function throwDice () {
         actionArea.innerHTML = '';
         gameData.roll1 = Math.floor(Math.random() * 6) + 1; // using ceiling could result in a zero 
         gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-        game.innerHTML = `<h3>Roll the dice, <strong>${gameData.players[gameData.index]}</strong></h3>`;
+        game.innerHTML = `<h2>Roll the dice, <strong>${gameData.players[gameData.index]}</strong></h2>`;
         game.innerHTML += `<img src="${gameData.dice[gameData.roll1-1]}"><img src="${gameData.dice[gameData.roll2-1]}">`;
         gameData.rollSum = gameData.roll1 + gameData.roll2;  
         console.log(gameData);
@@ -114,18 +100,18 @@
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
             // show the current score
             showCurrentScore();
-            setTimeout(setUpTurn, 3500);
+            setTimeout(setUpTurn, 3000);
         }
         // if either die is a 1...
         else if (gameData.roll1 === 1 || gameData.roll2 === 1) {
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p>Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</p>`;
-            setTimeout(setUpTurn, 3500);
+            game.innerHTML += `<p>Sorry, one of your rolls was a 1, switching to ${gameData.players[gameData.index]}</p>`;
+            setTimeout(setUpTurn, 3000);
         }
         //if either die is a 1...
         else {
             gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
+            actionArea.innerHTML = '<div id="roll_pass"><button id="rollagain">Roll again</button> or <button id="pass">Pass</button></div>';
 
             document.getElementById('rollagain').addEventListener('click', function () {
                 throwDice(); //gets you to throw the dice right away with the images showing so it doesn't disappear and you have to click it again to reappear 
@@ -138,12 +124,12 @@
 
             checkWinningCondition();
         }
-    }
+    };
 
     //checking winning condition
     function checkWinningCondition () {
         if (gameData.score[gameData.index] > gameData.gameEnd) {
-            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
+            score.innerHTML = `<h2 class="winner">${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
             
             // thank sound play
             thankSound.play();
@@ -155,10 +141,10 @@
             //show current score...
             showCurrentScore();
         }
-    }
+    };
 
     function showCurrentScore() {
-        score.innerHTML = `<h2 style="color:black;">Score</h2><h3><strong>${gameData.players[0]}: ${gameData.score[0]}</strong> | <strong>${gameData.players[1]}: ${gameData.score[1]}</strong></h3>`;
-    }
+        score.innerHTML = `<h2>Score</h2><h3><strong>${gameData.players[0]}: ${gameData.score[0]}</strong> | <strong>${gameData.players[1]}: ${gameData.score[1]}</strong></h3>`;
+    };
 
 })();
